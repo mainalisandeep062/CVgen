bfb53418b9f0e5da2fc69589dcc72e7272bf2691
Sandeep Mainali <sawondeep4@gmail.com>
2026-07-07 13:10:27 +0545

    first commit

---
+++ src/main/java/io/github/mainalisandeep/cvgen/CVgenApplication.java
@@ -0,0 +1,13 @@
+package io.github.mainalisandeep.cvgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CVgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mainalisandeep/cvgen/CVgenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mainalisandeep.cvgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CVgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
