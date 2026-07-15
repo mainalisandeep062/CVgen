477419ae21c51e51d1edc23e507b7dea94acaf37
Sandeep Mainali <sawondeep4@gmail.com>
2026-07-15 17:13:32 +0545

    new find methods

--- src/main/java/io/github/mainalisandeep/cvgen/repository/UserIdentityRepository.java
+++ src/main/java/io/github/mainalisandeep/cvgen/repository/UserIdentityRepository.java
@@ -2,8 +2,10 @@ package io.github.mainalisandeep.cvgen.repository;
 
 import io.github.mainalisandeep.cvgen.entity.UserIdentity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +13,7 @@ import java.util.UUID;
 public interface UserIdentityRepository extends JpaRepository<UserIdentity, UUID> {
 
     Optional<UserIdentity> findByProviderAndProviderId(String provider, String providerId);
+
+    @Query("SELECT ui FROM UserIdentity ui WHERE ui.user.id = :id")
+    List<UserIdentity> findByUserId(UUID id);
 }
--- src/main/java/io/github/mainalisandeep/cvgen/repository/UserRepository.java
+++ src/main/java/io/github/mainalisandeep/cvgen/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package io.github.mainalisandeep.cvgen.repository;
 
+import io.github.mainalisandeep.cvgen.dto.UserResponseDto;
 import io.github.mainalisandeep.cvgen.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,14 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT *
+                    FROM users u
+                    WHERE u.id = :id
+            """
+    )
+    User findByUserId(UUID id);
 }
